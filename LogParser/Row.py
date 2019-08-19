e083b20d98992ba2659772ec40d62a4eedb6afc9
John Murray <jmurray@ProcessingBox.isbe.local>
2019-08-19 11:46:08 -0600

    Parse the rest of the rows

--- LogParser/Row.py
+++ LogParser/Row.py
@@ -1,13 +1,11 @@
 import re
 import datetime
 
+
 class Row:
     """
     Parse row of a log, present data about it.
     """
-
-
-
     def __new__(cls, line):
 
         re_matchers = {}
@@ -39,32 +37,94 @@ class Row:
             \s-\s)?
             (.*)                    # Hit description
         """.format(re_matchers['base'])
-        re_matchers['generic'] = "{}".format(re_matchers['base'])
-        # print(re.match(Row.re_matchers['datetime'], line))
-        # print(re.search(Row.re_matchers['log_type'], line))
-        # print(re_matchers['datetime']+r"\ +"+re_matchers['log_type'])
-        if not re.search(re_matchers['base'], line, re.X):
+        re_matchers['combat_miss'] = r"""
+            {}
+            (.*)\smisses\syou\scompletely
+        """.format(re_matchers['base'])
+        re_matchers['combat_miss_2'] = r"""
+            {}
+            Your\s
+            (.*)
+            \smisses\s
+            (.*)
+            \scompletely\s-\s
+            (.*)
+        """.format(re_matchers['base'])
+        re_matchers['generic'] = r"""
+            {}
+            (.*)
+        """.format(re_matchers['base'])
+        re_matchers['bounty'] = r"""
+            {}
+            <font\ssize=\d+><b><color=0x\w+>
+            (.*)
+            \sISK
+        """.format(re_matchers['base'])
+        base_match = re.match(re_matchers['base'], line, re.X)
+
+        if not base_match:
             return None
-        match_object = re.search(re_matchers['combat_hit'], line, re.X)
-        if match_object:
-            # print(line)
-            return Combat(match_object)
+        if base_match.group(3) == 'combat':
+            match_object = re.search(re_matchers['combat_hit'], line, re.X)
+            if match_object:
+                # print(line)
+                return Combat(match_object)
+            #print(f"Match line \n{line}\n with \n{re_matchers['combat_miss']}")
+            match_object = re.match(re_matchers['combat_miss'], line, re.X)
+            if match_object:
+                return CombatMiss(match_object)
+            match_object = re.match(re_matchers['combat_miss_2'], line, re.X)
+            if match_object:
+                return CombatMiss(match_object)
+        if base_match.group(3) == 'bounty':
+            match_object = re.match(re_matchers['bounty'], line, re.X)
+            if match_object:
+                return Bounty(match_object)
         match_object = re.search(re_matchers['generic'], line, re.X)
-        if match_object and match_object.group(3) in ['notify', 'question', 'None', 'warning']:
+        if match_object and match_object.group(3) in ['notify', 'question', 'None', 'warning', 'hint', 'info']:
             return Generic(match_object)
         # print(re_matchers['combat_hit'])
         return Unknown(match_object)
 
 
-class Combat:
-    pass
+class RowBase:
     def __init__(self, match_object):
-
         self.date = match_object.group(1)
         self.time = match_object.group(2)
 
         dt_string = f"{self.date} {self.time}"
         self.datetime = datetime.datetime.strptime(dt_string, "%Y.%m.%d %H:%M:%S")
+
+
+class Bounty(RowBase):
+    def __init__(self, match_object):
+        """ TODO: Parse this out
+        """
+        super().__init__(match_object)
+        self.isk = float(match_object.group(4).replace(',','_'))
+        pass
+
+
+class CombatMiss(RowBase):
+    def __init__(self, match_object):
+        """ TODO: Parse this out
+        """
+        super().__init__(match_object)
+        pass
+
+
+class CombatMiss(RowBase):
+    def __init__(self, match_object):
+        """ TODO: Parse this out
+        """
+        super().__init__(match_object)
+        pass
+
+
+class Combat(RowBase):
+    pass
+    def __init__(self, match_object):
+        super().__init__(match_object)
 
         self.amount = int(match_object.group(4))
         self.to = match_object.group(5) == 'to'
@@ -76,20 +136,14 @@ class Combat:
     def __str__(self):
         return f"{self.datetime} {self.amount} {'to' if self.to else 'from'} @ {self.source_target} "
 
-class Unknown:
+
+class Unknown(RowBase):
     def __init__(self, match_object):
+        super().__init__(match_object)
 
-        self.date = match_object.group(1)
-        self.time = match_object.group(2)
 
-        dt_string = f"{self.date} {self.time}"
-        self.datetime = datetime.datetime.strptime(dt_string, "%Y.%m.%d %H:%M:%S")
-
-class Generic:
+class Generic(RowBase):
     def __init__(self, match_object):
+        super().__init__(match_object)
+        self.message = match_object.group(4)
 
-        self.date = match_object.group(1)
-        self.time = match_object.group(2)
-
-        dt_string = f"{self.date} {self.time}"
-        self.datetime = datetime.datetime.strptime(dt_string, "%Y.%m.%d %H:%M:%S")
--- test.py
+++ test.py
@@ -9,16 +9,19 @@ from pathlib import Path
 from LogParser import Row, Parser
 
 
-logs_location = Path("/media/sf_C_DRIVE/Users/John/Documents/EVE/logs/GameLogs/20190818_212723.txt")
+logs_location = Path("/media/sf_C_DRIVE/Users/John/Documents/EVE/logs/GameLogs/")
+
+latest_log = sorted(logs_location.glob("*.txt"), key=lambda x: x.stat().st_mtime, reverse=True)[0]
 
 # latest_log = sorted(logs_location.glob("*.txt"), key=lambda x: x.stat().st_mtime, reverse=True)[0]
 
 
-with open(logs_location, 'r') as fp:
+with open(latest_log, 'r') as fp:
     for line in fp:
         # print(line,end='')
         r = Row.Row(line)
-        if type(r) == Row.Unknown:
-            print(line)
+        if type(r) == Row.Generic:
+            print(line, end='')
+
 
 
