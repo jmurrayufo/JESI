365ac9efc4cbc764d0843623c405287e4c9070c0
John Murray <jmurray@ProcessingBox.isbe.local>
2019-08-19 16:23:43 -0600

    Move display to it's own class

---
+++ LogParser/Display.py
@@ -0,0 +1,75 @@
+from os import system, name 
+from datetime import timedelta
+
+
+class Display:
+
+
+    def __init__(self, args, parser):
+        self.args = args
+        self.parser = parser
+
+
+    def display(self):
+        # Clear screen for display
+        if name == 'nt': 
+            system('cls') 
+        else: 
+            system('clear') 
+
+        # Loop through all but the last (special) timebox
+        for time_box in self.args.time_boxes[:-1]:
+            print(f"Time Delta: {time_box}")
+            # Handy data shortcuts
+            data = self.parser.data[time_box] 
+            box_seconds = max(1,time_box.total_seconds())
+            # TODO: Handle final timebox with some sanity
+            print(f"  Total Damage Sent: {data['damage_to']:,.0f} ({data['damage_to']/box_seconds:,.1f} dps)")
+            print(f"  Total Damage Tanked: {data['damage_from']:,.0f} ({data['damage_from']/box_seconds:,.1f} dps)")
+            #print(f"  Bounties Collected: {data['isk']} ISK")
+            print(f"  Bounties Collected: {data['isk']:,.2f} ISK ({data['isk']/box_seconds*60*60/1e6:,.2f} Misk/hr)")
+            
+            if self.args.box_totals > 0:
+                new_lines = "" if self.args.box_totals > 1 else "\n"
+                self._sub_display(data['damage_ship_to'], "  Top Damage Done Target:", 1, new_lines)
+                self._sub_display(data['damage_ship_from'], "  Top Damage Recieved Source:", 1, new_lines)
+                self._sub_display(data['damage_type_to'], "  Top Damage Type Dealt:", 1, new_lines)
+                self._sub_display(data['damage_type_from'], "  Top Damage Type Recieved:", 1, new_lines)
+                self._sub_display(data['damage_system_to'], "  Top Damage System Dealt:", 1, new_lines)
+                self._sub_display(data['damage_system_from'], "  Top Damage System Recieved:", 1, new_lines)
+
+                if len(list(data['damage_system_from'].keys())):
+                    print(f"  Top Damage System Recieved:", end="")
+                    for _system in list(data['damage_system_from'].keys())[:1]:
+                        print(f" {_system} ({data['damage_system_from'][_system]:,.0f})")
+            print()
+
+        session_seconds =  max(1, (self.parser._last_dt - self.parser._first_dt ).total_seconds())
+
+        for time_box in self.args.time_boxes[-1:]:
+            print(f"Time Delta: {timedelta(seconds=session_seconds)} (Current Session)")
+            # Handy data shortcuts
+            data = self.parser.data[time_box] 
+            box_seconds = session_seconds
+            # TODO: Handle final timebox with some sanity
+            print(f"  Total Damage Sent: {data['damage_to']:,.0f} ({data['damage_to']/box_seconds:,.1f} dps)")
+            print(f"  Total Damage Tanked: {data['damage_from']:,.0f} ({data['damage_from']/box_seconds:,.1f} dps)")
+            print(f"  Bounties Collected: {data['isk']:,.2f} ISK ({data['isk']/box_seconds*60*60/1e6:,.2f} Misk/hr)")
+            
+            if self.args.box_totals > 0:
+                new_lines = "" if self.args.box_totals > 1 else "\n"
+                new_lines = "\n"
+                self._sub_display(data['damage_ship_to'], "  Top Damage Done Target:", self.args.box_totals, new_lines)
+                self._sub_display(data['damage_ship_from'], "  Top Damage Recieved Source:", self.args.box_totals, new_lines)
+                self._sub_display(data['damage_type_to'], "  Top Damage Type Dealt:", self.args.box_totals, new_lines)
+                self._sub_display(data['damage_type_from'], "  Top Damage Type Recieved:", self.args.box_totals, new_lines)
+                self._sub_display(data['damage_system_to'], "  Top Damage System Dealt:", self.args.box_totals, new_lines)
+                self._sub_display(data['damage_system_from'], "  Top Damage System Recieved:", self.args.box_totals, new_lines)
+
+
+    def _sub_display(self, sub_data, caption, box_totals, endl):
+        if len(list(sub_data.keys())):
+            print(caption, end=endl)
+            total = sum(sub_data.values())
+            for ship in list(sub_data.keys())[:box_totals]:
+                print(f"    {sub_data[ship]:12,.0f} {ship} ({sub_data[ship]/total:.1%})")
--- LogParser/__main__.py
+++ LogParser/__main__.py
@@ -1,6 +1,5 @@
 
 from datetime import timedelta
-from os import system, name 
 from pathlib import Path
 import argparse
 import datetime
@@ -9,7 +8,7 @@ import pprint
 import re
 import time
 
-from . import Row, Parser
+from . import Row, Parser, Display
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-n', dest='refresh', default=5, type=float,
@@ -25,6 +24,17 @@ parser.add_argument('-s', '--summary', action='store_true', default=False,
     help='Just display a summery and exit')
 
 args = parser.parse_args()
+args.time_boxes = [
+    # timedelta(seconds=10),
+    timedelta(minutes=1),
+    timedelta(minutes=5),
+    # timedelta(minutes=30),
+    timedelta(minutes=60),
+    timedelta.max,
+]
+
+if args.summary:
+    args.box_totals = 9999
 
 if args.verbose > 0:
     print(args)
@@ -35,122 +45,21 @@ assert args.refresh >= 1, "Refresh rate too high, please pick a more sane value!
 
 def main_loop(args):
     
-    box_totals = args.box_totals if args.summary == False else 9999
 
     while 1:
         logs_location = Path(args.logpath)
         latest_log = sorted(logs_location.glob("*.txt"), key=lambda x: x.stat().st_mtime, reverse=True)[0]
 
         parser = Parser.Parser(latest_log)
-
-        time_boxes = [
-            # timedelta(seconds=10),
-            timedelta(minutes=1),
-            timedelta(minutes=5),
-            # timedelta(minutes=30),
-            timedelta(minutes=60),
-            timedelta.max,
-            ]
-
-        parser.parse(time_boxes)
-
-        # Clear screen for display
-        if name == 'nt': 
-            system('cls') 
-        else: 
-            system('clear') 
-
-        # Loop through all but the last (special) timebox
-        for time_box in time_boxes[:-1]:
-            print(f"Time Delta: {time_box}")
-            # Handy data shortcuts
-            data = parser.data[time_box] 
-            box_seconds = max(1,time_box.total_seconds())
-            # TODO: Handle final timebox with some sanity
-            print(f"  Total Damage Sent: {data['damage_to']:,.0f} ({data['damage_to']/box_seconds:,.1f} dps)")
-            print(f"  Total Damage Tanked: {data['damage_from']:,.0f} ({data['damage_from']/box_seconds:,.1f} dps)")
-            #print(f"  Bounties Collected: {data['isk']} ISK")
-            print(f"  Bounties Collected: {data['isk']:,.2f} ISK ({data['isk']/box_seconds*60*60/1e6:,.2f} Misk/hr)")
-            
-            if args.box_totals > 0:
-                new_lines = "" if args.box_totals > 1 else "\n"
-                if len(list(data['damage_ship_to'].keys())):
-                    print(f"  Top Damage Done Target:", end=new_lines)
-                    for ship in list(data['damage_ship_to'].keys())[:1]:
-                        print(f" {ship} ({data['damage_ship_to'][ship]:,.0f})")
-                if len(list(data['damage_ship_from'].keys())):
-                    print(f"  Top Damage Recieved Source:", end="")
-                    for ship in list(data['damage_ship_from'].keys())[:1]:
-                        print(f" {ship} ({data['damage_ship_from'][ship]:,.0f})")
-
-                if len(list(data['damage_type_to'].keys())):
-                    print(f"  Top Damage Type Dealt:", end="")
-                    for _type in list(data['damage_type_to'].keys())[:1]:
-                        print(f" {_type} ({data['damage_type_to'][_type]:,.0f})")
-                if len(list(data['damage_type_from'].keys())):
-                    print(f"  Top Damage Type Recieved:", end="")
-                    for _type in list(data['damage_type_from'].keys())[:1]:
-                        print(f" {_type} ({data['damage_type_from'][_type]:,.0f})")
-
-                if len(list(data['damage_system_to'].keys())):
-                    print(f"  Top Damage System Dealt:", end="")
-                    for _system in list(data['damage_system_to'].keys())[:1]:
-                        print(f" {_system} ({data['damage_system_to'][_system]:,.0f})")
-                if len(list(data['damage_system_from'].keys())):
-                    print(f"  Top Damage System Recieved:", end="")
-                    for _system in list(data['damage_system_from'].keys())[:1]:
-                        print(f" {_system} ({data['damage_system_from'][_system]:,.0f})")
-            print()
-
-        session_seconds =  max(1, (parser._last_dt - parser._first_dt ).total_seconds())
-
-        for time_box in time_boxes[-1:]:
-            print(f"Time Delta: {timedelta(seconds=session_seconds)} (Current Session)")
-            # Handy data shortcuts
-            data = parser.data[time_box] 
-            box_seconds = session_seconds
-            # TODO: Handle final timebox with some sanity
-            print(f"  Total Damage Sent: {data['damage_to']:,.0f} ({data['damage_to']/box_seconds:,.1f} dps)")
-            print(f"  Total Damage Tanked: {data['damage_from']:,.0f} ({data['damage_from']/box_seconds:,.1f} dps)")
-            print(f"  Bounties Collected: {data['isk']:,.2f} ISK ({data['isk']/box_seconds*60*60/1e6:,.2f} Misk/hr)")
-            
-            if args.box_totals > 0:
-                new_lines = "" if args.box_totals > 1 else "\n"
-
-                sub_data = data['damage_ship_to']
-                if len(list(sub_data.keys())):
-                    print(f"  Top Damage Done Target:")
-                    total = sum(sub_data.values())
-                    for ship in list(sub_data.keys())[:box_totals]:
-                        print(f"    {sub_data[ship]:12,.0f} {ship} ({sub_data[ship]/total:.1%})")
-
-                if len(list(data['damage_ship_from'].keys())):
-                    print(f"  Top Damage Recieved Source:")
-                    for ship in list(data['damage_ship_from'].keys())[:box_totals]:
-                        print(f"    {ship} ({data['damage_ship_from'][ship]:,.0f})")
-
-                if len(list(data['damage_type_to'].keys())):
-                    print(f"  Top Damage Type Dealt:")
-                    for _type in list(data['damage_type_to'].keys())[:box_totals]:
-                        print(f"    {_type} ({data['damage_type_to'][_type]:,.0f})")
-
-                if len(list(data['damage_type_from'].keys())):
-                    print(f"  Top Damage Type Recieved:")
-                    for _type in list(data['damage_type_from'].keys())[:box_totals]:
-                        print(f"    {_type} ({data['damage_type_from'][_type]:,.0f})")
-
-                if len(list(data['damage_system_to'].keys())):
-                    print(f"  Top Damage System Dealt:")
-                    for _system in list(data['damage_system_to'].keys())[:box_totals]:
-                        print(f"    {_system} ({data['damage_system_to'][_system]:,.0f})")
-
-                if len(list(data['damage_system_from'].keys())):
-                    print(f"  Top Damage System Recieved:")
-                    for _system in list(data['damage_system_from'].keys())[:box_totals]:
-                        print(f"    {_system} ({data['damage_system_from'][_system]:,.0f})")
+        parser.parse(args.time_boxes)
+        display = Display.Display(args, parser)
+        display.display()
 
         if args.summary:
             break
         time.sleep(args.refresh)
 
 main_loop(args)
+
+
+
